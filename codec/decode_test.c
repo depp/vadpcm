2e05c8724fee695a3513958c2668c30f7365f3ea
Dietrich Epp <depp@zdome.net>
2023-09-21 23:13:28 -0400

    Style: clang-format

--- codec/decode_test.c
+++ codec/decode_test.c
@@ -1,8 +1,8 @@
 // Copyright 2022 Dietrich Epp.
 // This file is part of VADPCM. VADPCM is licensed under the terms of the
 // Mozilla Public License, version 2.0. See LICENSE.txt for details.
-#include "codec/vadpcm.h"
 #include "codec/test.h"
+#include "codec/vadpcm.h"
 
 #include <stdio.h>
 #include <stdlib.h>
