ee5ad18ae5fb9054bfb8bffccfc8c308175e24bf
Dietrich Epp <code-author@moria.us>
2025-09-15 15:16:20 -0400

    Create program for preprocessing test files
    This program converts input audio files to monophonic WAV at a selection
    of different sample rates. This is done by invoking FFmpeg, which is
    good enough for testing with.

---
+++ scripts/convert.py
@@ -0,0 +1,76 @@
+import argparse
+import pathlib
+import re
+import subprocess
+import sys
+
+from typing import List, Tuple
+
+ALPHANUM = re.compile(r"[a-zA-Z0-9]+")
+
+
+def simplify_name(name: str) -> str:
+    return "_".join(ALPHANUM.findall(name.replace("'", "")))
+
+
+def main() -> None:
+    p = argparse.ArgumentParser("convert.py", allow_abbrev=False)
+    p.add_argument("-input", required=True, help="Input directory", type=pathlib.Path)
+    p.add_argument("-output", required=True, help="Output directory", type=pathlib.Path)
+    args = p.parse_args()
+    input_dir: pathlib.Path = args.input
+    output_dir: pathlib.Path = args.output
+
+    output_dir.mkdir(exist_ok=True)
+    rates: List[int] = [44100, 32000, 22050, 16000, 11025]
+    rate_dirs: List[Tuple[int, pathlib.Path]] = []
+    for rate in rates:
+        rate_dir = output_dir / str(rate)
+        rate_dir.mkdir(exist_ok=True)
+        rate_dirs.append((rate, rate_dir))
+    files: List[pathlib.Path] = []
+    names = set()
+    for item in input_dir.iterdir():
+        if item.name.startswith("."):
+            continue
+        name = simplify_name(item.stem)
+        if not name:
+            print(
+                "Warning: Skipping file with special characters:",
+                repr(name),
+                file=sys.stderr,
+            )
+            continue
+        if name in names:
+            print("Warning: Skipping duplicate name:", name, file=sys.stderr)
+            continue
+        names.add(name)
+        wav_name = name + ".wav"
+        for rate, rate_dir in rate_dirs:
+            wav_file = rate_dir / wav_name
+            proc = subprocess.run(
+                [
+                    "ffmpeg",
+                    "-hide_banner",
+                    "-loglevel",
+                    "error",
+                    "-y",
+                    "-i",
+                    item,
+                    "-ac",
+                    "1",
+                    "-ar",
+                    str(rate),
+                    wav_file,
+                ]
+            )
+            if proc.returncode:
+                print(
+                    f"Error: ffmpeg failed with status {proc.returncode}",
+                    file=sys.stderr,
+                )
+                raise SystemExit(1)
+
+
+if __name__ == "__main__":
+    main()
