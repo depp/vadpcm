5e3befafc89a3e37605bb58e9ae9ec13c7f929b1
Dietrich Epp <code-author@moria.us>
2026-01-15 00:37:06 -0500

    MSVC: Implement swap16

--- common/binary.c
+++ common/binary.c
@@ -3,6 +3,10 @@
 // Mozilla Public License, version 2.0. See LICENSE.txt for details.
 #include "common/binary.h" // IWYU pragma: keep
 
+#if _MSC_VER
+#include <stdlib.h>
+#endif
+
 uint16_t read16be(const uint8_t *ptr);
 uint32_t read32be(const uint8_t *ptr);
 uint64_t read64be(const uint8_t *ptr);
@@ -22,8 +26,25 @@ void swap16le_inplace(void *ptr, size_t size);
 void swap16be(void *dest, const void *src, size_t size);
 void swap16be_inplace(void *ptr, size_t size);
 
+#if __GNUC__
 void swap16(void *dest, const void *src, size_t size) {
     for (size_t i = 0; i < size; i++) {
         ((uint16_t *)dest)[i] = __builtin_bswap16(((const uint16_t *)src)[i]);
     }
 }
+#elif _MSC_VER
+void swap16(void *dest, const void *src, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        ((uint16_t *)dest)[i] = _byteswap_ushort(((const uint16_t *)src)[i]);
+    }
+}
+#else
+void swap16(void *dest, const void *src, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        uint8_t x = ((const uint8_t *)src)[2 * i];
+        uint8_t y = ((const uint8_t *)src)[2 * i + 1];
+        ((uint8_t *)dest)[2 * i] = y;
+        ((uint8_t *)dest)[2 * i + 1] = x;
+    }
+}
+#endif
