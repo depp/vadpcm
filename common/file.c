947057db0d14d102325d81e772ac07a99c0c60cd
Dietrich Epp <code-author@moria.us>
2026-01-15 02:39:15 -0500

    Windows: Port file I/O

--- common/file.c
+++ common/file.c
@@ -1,6 +1,92 @@
 // Copyright 2026 Dietrich Epp.
 // This file is part of VADPCM. VADPCM is licensed under the terms of the
 // Mozilla Public License, version 2.0. See LICENSE.txt for details.
+
+#if _WIN32
+
+#define WIN32_LEAN_AND_MEAN
+
+#include "common/defs.h"
+#include "common/util.h"
+
+#include <Windows.h>
+#include <stdlib.h>
+
+void input_file_destroy(struct input_file *file) {
+    free(file->data);
+}
+
+int input_file_read(struct input_file *file, const char *filename) {
+    HANDLE h = CreateFileA(filename, FILE_READ_DATA, FILE_SHARE_READ, NULL,
+                           OPEN_EXISTING, 0, NULL);
+    if (h == INVALID_HANDLE_VALUE) {
+        LOG_ERROR_ERRNO(GetLastError(), "could not open file");
+        return -1;
+    }
+    BY_HANDLE_FILE_INFORMATION info;
+    if (!GetFileInformationByHandle(h, &info)) {
+        LOG_ERROR_ERRNO(GetLastError(), "could not get file information");
+        goto error;
+    }
+    if (info.nFileSizeHigh != 0) {
+        LOG_ERROR("file too large");
+        goto error;
+    }
+    DWORD size = info.nFileSizeLow;
+    void *data = malloc(size);
+    if (data == NULL) {
+        LOG_ERROR("out of memory");
+        goto error;
+    }
+    DWORD bytes_read;
+    if (!ReadFile(h, data, size, &bytes_read, NULL)) {
+        LOG_ERROR_ERRNO(GetLastError(), "could not read file");
+        goto error;
+    }
+    *file = (struct input_file){
+        .data = data,
+        .size = size,
+    };
+    return 0;
+error:
+    CloseHandle(h);
+    return -1;
+}
+
+int output_file_write(const char *filename, const struct byteslice *data,
+                      size_t count) {
+    HANDLE h = CreateFileA(filename, FILE_WRITE_DATA, 0, NULL, CREATE_ALWAYS,
+                           FILE_ATTRIBUTE_NORMAL, NULL);
+    if (h == INVALID_HANDLE_VALUE) {
+        LOG_ERROR_ERRNO(GetLastError(), "could not create file");
+        return -1;
+    }
+    for (size_t i = 0; i < count; i++) {
+        if (data[i].size > 0) {
+            DWORD size = (DWORD)data[i].size;
+            DWORD bytes_written;
+            if (!WriteFile(h, data[i].ptr, size, &bytes_written, NULL)) {
+                LOG_ERROR_ERRNO(GetLastError(), "could not write");
+                goto error;
+            }
+            if (bytes_written < size) {
+                LOG_ERROR("incomplete write");
+                goto error;
+            }
+        }
+    }
+    if (!CloseHandle(h)) {
+        LOG_ERROR("could not write");
+        return -1;
+    }
+    return 0;
+error:
+    CloseHandle(h);
+    return -1;
+}
+
+#else
+
 #define _FILE_OFFSET_BITS 64
 #define _DEFAULT_SOURCE 1
 
@@ -116,3 +202,5 @@ error:
     LOG_ERROR_ERRNO(errcode, "could not write");
     return -1;
 }
+
+#endif
